ee816e68da720bddd1ab6fd6cbf4da5be405982c
alire-alavi <alirezaalavirazavi@hotmail.com>
2025-11-04 18:43:35 +0330

    add: config service read env ability

--- apps/jb-auth/src/app/app.module.ts
+++ apps/jb-auth/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { PrismaModule } from './prisma/prisma.module';
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule,
     PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
