0ee819bcdd11e4ea9777b0665f1d868bc336a4fd
alire-alavi <alirezaalavirazavi@hotmail.com>
2025-11-04 19:09:22 +0330

    add: auth module

--- apps/jb-auth/src/app/app.module.ts
+++ apps/jb-auth/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { PrismaModule } from './prisma/prisma.module';
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { UsersModule } from './users/users.module';
       autoSchemaFile: true,
     }),
     UsersModule,
+    AuthModule,
   ],
   providers: [],
 })
