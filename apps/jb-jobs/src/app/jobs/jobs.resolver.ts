60da5272e63c95e9e4882840e22a863fb7da90dd
alire-alavi <alirezaalavirazavi@hotmail.com>
2025-11-24 14:17:21 +0330

    update: test gRPC gql auth guard

--- apps/jb-jobs/src/app/jobs/jobs.resolver.ts
+++ apps/jb-jobs/src/app/jobs/jobs.resolver.ts
@@ -2,17 +2,21 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { Job } from './models/jobs.model';
 import { JobsService } from './jobs.service';
 import { ExecuteJobInput } from './dto/exectue-job.input';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '@jaaber/nestjs';
 
 @Resolver()
 export class JobsResolver {
   constructor(private readonly jobsService: JobsService) {}
 
   @Query(() => [Job], { name: 'jobs' })
+  @UseGuards(GqlAuthGuard)
   async getJobs() {
     return this.jobsService.getJobs();
   }
 
   @Mutation(() => Job)
+  @UseGuards(GqlAuthGuard)
   async executeJob(@Args('executeJobInput') exectueJobInput: ExecuteJobInput) {
     return this.jobsService.executeJob(exectueJobInput.name);
   }
