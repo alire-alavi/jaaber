607fa4cb734991c55eea4cd80cd01a5763902c4f
alire-alavi <alirezaalavirazavi@hotmail.com>
2025-11-24 14:15:58 +0330

    add: grpc gql auth guard

---
+++ libs/nestjs/src/lib/guards/gql-auth.guard.ts
@@ -0,0 +1,55 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import {
+  AUTH_PACKAGE_NAME,
+  AUTH_SERVICE_NAME,
+  AuthServiceClient,
+} from '@jaaber/grpc';
+import { Observable, catchError, map, of } from 'rxjs';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+@Injectable()
+export class GqlAuthGuard implements CanActivate, OnModuleInit {
+  private readonly logger = new Logger(GqlAuthGuard.name);
+  private authService: AuthServiceClient;
+
+  constructor(@Inject(AUTH_PACKAGE_NAME) private client: ClientGrpc) {}
+
+  onModuleInit() {
+    this.authService =
+      this.client.getService<AuthServiceClient>(AUTH_SERVICE_NAME);
+  }
+
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const token = this.getRequest(context).cookies?.Authentication;
+
+    if (!token) {
+      return false;
+    }
+
+    return this.authService.authenticate({ token }).pipe(
+      map((res) => {
+        this.logger.log(res);
+        return true;
+      }),
+      catchError((err) => {
+        this.logger.log(err);
+        return of(false);
+      })
+    );
+  }
+
+  private getRequest(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext().req;
+  }
+}
